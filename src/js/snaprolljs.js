cbf46973053ae0ec89f79ca470734cc20eac03f0
Wil <wilfram2005@gmail.com>
2025-10-25 11:24:39 -0500

    fix: Update version number to 2.0.4 in package.json, readme.md, and snaprolljs.js

--- src/js/snaprolljs.js
+++ src/js/snaprolljs.js
@@ -1,7 +1,7 @@
 /**
  * @file snaproll.js
  * @author [Wil]
- * @version 2.0.3
+ * @version 2.0.4
  * @description A lightweight, dependency-free library for creating full-page, snap-scrolling presentations with configurable animations for vertical sections and horizontal slides.
  */
 
